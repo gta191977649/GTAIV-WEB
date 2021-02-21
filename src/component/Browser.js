b3f7fa9a41c755d6b4f19467c767ea87789c97e3
ヌルポ <Le.Cai-1@student.uts.edu.au>
2021-02-22 04:05:27 +1100

    Update Browser.js

--- src/component/Browser.js
+++ src/component/Browser.js
@@ -2,19 +2,35 @@ import "./Browser.css"
 import React, { Component } from 'react'
 import Iframe from 'react-iframe'
 export default class browser extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            buffer:"https://www.bilibili.com",
+            url:""
+        }
+    }
+    componentDidMount(){
+        this.setState({url:this.state.buffer})
+    }
     render() {
         return (
-            <div className="browser">
+            <div className="browser" tabIndex="0" onKeyDown={(e)=>{
+                if (e.code === "Enter") {
+                    this.setState({url:this.state.buffer})
+                }
+            }}>
                 <div className="header">
                     <div className="controls">
-                        <input type="text" value="https://www.bilibili.com"/>
+                        <input type="text" value={this.state.buffer} onChange={(e)=>{
+                            this.setState({buffer:e.target.value})
+                        }}/>
                         <a className="btn_nav_prev" href="#"></a>
                         <a className="btn_nav_next" href="#"></a>
                         <a className="btn_mail" href="#"></a>
                         <a className="btn_home" href="#"></a>
                     </div>
                 </div>
-                <Iframe url="https://www.bilibili.com/"
+                <Iframe url={this.state.url}
                     width="100%"
                     height="100%"
                     id="myId"
