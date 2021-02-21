03a3cf5b0dc411e6844b5a0cca1b214c1e8f05d2
ヌルポ <Le.Cai-1@student.uts.edu.au>
2021-02-22 03:57:54 +1100

    finalise

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import icon_web from "./img/icon/browser.png"
+import logo from "./img/ui/tw.png"
 import log_out from "./img/icon/logout.png"
 import Browser from "./component/Browser"
 import click from './sfx/click.mp3';
@@ -22,6 +23,7 @@ function App() {
         <div className="desktop" tabIndex="0" onKeyDown={onKey} onClick={()=>{audio.play()}}>
        
           <div className="desktop-container">
+          <img src={logo} className="logo"/>
             <div className="grid">
                 <li onClick={openBrowser}>
                   <img src={icon_web}/>
